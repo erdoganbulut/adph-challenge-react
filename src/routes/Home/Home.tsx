9e70fa2e0c478a22fea53f05753c8bae902c0668
Erdoğan Bulut <erdoganbulut@Erdogan-MacBook-Pro.local>
2019-05-14 12:56:04 +0300

    Fix home for unset props and fix get redux state

--- src/routes/Home/Home.tsx
+++ src/routes/Home/Home.tsx
@@ -5,30 +5,31 @@ import Props from './props.types';
 import StateI, { DatasetItemI } from '../../store/reducers/state.types';
 import DatasetItem from '../../components/DatasetItem/DatasetItem';
 import handleUnflattenArr from '../../helpers/unflatten';
+import { API_CALL_REQUEST } from '../../store/actions/actions.types';
 
 const mapStateToProps = (state: StateI) => {
   return {
-    fetching: state.fetching,
-    dataset: handleUnflattenArr(state.dataset),
-    error: state.error,
+    dataset: state.dataset,
   };
 };
 
 const mapDispatchToProps = (dispatch: Dispatch) => {
   return {
-    onRequestDataset: () => dispatch({ type: 'API_CALL_REQUEST' }),
+    onRequestDataset: () => dispatch({ type: API_CALL_REQUEST }),
   };
 };
 
 const Home: React.FC<Props> = props => {
-  const { fetching, dataset, onRequestDataset, error } = props;
+  const { dataset, onRequestDataset } = props;
+
+  const unflatdataset = handleUnflattenArr(dataset);
 
   useEffect(() => {
     onRequestDataset();
   }, []);
   return (
     <div>
-      {dataset.map(item => {
+      {unflatdataset.map(item => {
         return <DatasetItem key={item.ID} data={item} />;
       })}
     </div>
